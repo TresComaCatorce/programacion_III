0d5096e41ff77c47d9a64304c586b40d1c81942a
alumno <alumno>
2018-04-24 22:16:19 -0300

    Clase 24-04-2018 Modelo de Primer Parcial Laboratorio III

--- 2018-04-24_Clase_09/TP Blog/scripts/index.js
+++ 2018-04-24_Clase_09/TP Blog/scripts/index.js
@@ -1,14 +1,60 @@
+//-----------------------------------
+// Declaracion de variables globales.
+//-----------------------------------
 var xhr;
 var datos = new Array();
+//-----------------------------------
 
-function cargarDatos(){
 
-    
-}
 
-window.onload = function(){
+//--------------------------------------------------------------------------------
+// Funciones que se ejecutan cuando carga la pagina (Una vez cargado todo el DOM).
+//--------------------------------------------------------------------------------
+window.onload = function()
+{
     cargarDatos();
-    
 }
+//--------------------------------------------------------------------------------
 
 
+
+//------------------------------------------------------------------------
+// Traigo los articulos del blog desde el servidor con una peticion AJAX.
+//------------------------------------------------------------------------
+function cargarDatos()
+{
+    xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function()
+    {
+        if (this.readyState == 4 && this.status == 200)
+        {
+           var resp = JSON.parse(this.response);
+           console.log(resp.message, resp.data);
+           refrescarTabla(resp.data);
+           datos = resp.data;
+        }
+    };
+    xhr.open("POST","http://localhost:3000/traer",true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(JSON.stringify({"collection":"posts"}));
+}
+//------------------------------------------------------------------------
+
+
+
+//--------------------------------------------------------------------------------
+// Funcion que dibuja la tabla de articulos existententes en la pagina.
+// @param <arrayArticulos> Array de objetos javascript con los articulos del blog.
+//--------------------------------------------------------------------------------
+function refrescarTabla( arrayArticulos )
+{
+    if(arrayArticulos)
+    {
+        arrayArticulos.forEach(function(item)
+        {
+            articuloHTML = '<div class="row">'+ item.titulo + '</div><div class="row">' + item.articulo + '</div><div class="row">' + item.created_dttm + '</div>';
+            $('#articulos').html(articuloHTML);
+        });
+    }
+}
+//--------------------------------------------------------------------------------
